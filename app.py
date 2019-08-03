5ee2931fd352a2d041f0a01a4991ace22e549e9a
Serg <s.urvanov@gmail.com>
2019-08-03 23:32:34 +0300

    Change design main page and upload file.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from flask import (Flask, render_template, Response, request, flash, request, redirect, url_for, render_template,
-                    session, g)
+                    session, g, jsonify)
 
 from werkzeug.utils import secure_filename
 import logging
@@ -26,21 +26,49 @@ def index_src():
     return render_template('index_src.html')
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
 def index():
     """Video streaming home page."""
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+    return render_template('index.html')
+
+
+@app.route('/add_files', methods=['POST'])
+def add_files():
+    trace1(__file__, sys._getframe().f_lineno, __name__, os.getpid(), os.getppid(), current_process().name)
+    logging.getLogger().info("add_request")
+
+    logging.getLogger().info(request.form.to_dict())
+    count_error = -1
+    error = True
+    while error and count_error <= 5:
+        try:
+            error = False
+        except Exception as err:
+            count_error += 1
+            error = True
+            logging.getLogger().debug(f"count_error={count_error}  Error:{err} ")
+    logging.getLogger().debug("")
+
+
+    logging.getLogger().debug(request.form.to_dict())
+    if len(request.files) > 0:
+        submitted_file = request.files['upload']
+    else:
+        submitted_file = None
+    if submitted_file is not None:
+        filename = submitted_file.filename
+
+    if submitted_file is not None:
+        if allowed_file(filename):
+            filename = secure_filename(filename)
             full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             if os.path.exists(full_path):
                 os.remove(full_path)
-            file.save(full_path)
+            submitted_file.save(full_path)
             return redirect(url_for('index', filename=filename))
-    return render_template('index.html')
-
+    else:
+        return render_template(url_for('index', title='Файл не отправлен'), 201)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
